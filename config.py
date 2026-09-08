27fef669a097d95b8d1426705a92f376d25f7a82
VictorCourtois135 <victorcourtois135@gmail.com>
2026-09-08 10:18:56 +0200

    adding config and first parser

---
+++ config.py
@@ -0,0 +1,8 @@
+url = "https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=440&count=3"
+output_file1 = "out.json"
+output_file2 = "out.txt"
+
+
+backup = "https://steamspy.com/api.php?request=top100in2weeks"
+
+prefix_url = "api.steampowered.com"
